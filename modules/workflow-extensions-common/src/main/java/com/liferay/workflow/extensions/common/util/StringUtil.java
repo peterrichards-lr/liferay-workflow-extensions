2fff42b51d8b496a641f91fc2e472f2b72d9c8cc
Peter Richards <peter.richards@liferay.com>
2023-05-12 13:16:57 +0100

    Added methods to build localisation map

--- modules/workflow-extensions-common/src/main/java/com/liferay/workflow/extensions/common/util/StringUtil.java
+++ modules/workflow-extensions-common/src/main/java/com/liferay/workflow/extensions/common/util/StringUtil.java
@@ -1,7 +1,21 @@
 package com.liferay.workflow.extensions.common.util;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
 public final class StringUtil {
     public static boolean isBlank(final String string) {
         return string == null || string.trim().isEmpty();
+    }
+
+    public static Map<Locale, String> localiseString(final String value) {
+        return localiseString(value, Locale.ROOT);
+    }
+
+    public static Map<Locale, String> localiseString(final String value, final Locale locale) {
+        return new HashMap<>() {{
+            put(locale, value);
+        }};
     }
 }
